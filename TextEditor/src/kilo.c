2b90b28f8fa8beb9c3c8fe28e907a50db40c902c
SammyAlsadek <sw.alsadek@gmail.com>
2021-12-07 04:52:36 +0000

    Completed 'Clear the screen on exit' Step. SA

--- TextEditor/src/kilo.c
+++ TextEditor/src/kilo.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -74,6 +77,8 @@ void editorProcessKeypress()
     switch (c)
     {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
     }
